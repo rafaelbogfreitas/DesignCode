918fa8b017a1a36bde4f84312433670ba4abfee7
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-18 23:50:31 -0300

    Finish develop the first card

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -16,15 +16,25 @@ struct ContentView: View {
                     Text("UI Design")
                         .font(.title)
                         .fontWeight(.semibold)
+                        .foregroundColor(.white)
                     Text("Certificate")
                         .foregroundColor(Color("accent"))
                 }
                 Spacer()
                 Image("Logo1")
             }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            
             Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
         }
-        
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
 
