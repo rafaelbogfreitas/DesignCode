c17bca3eca240d6700ce4dfeb255a4713116cdac
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-20 23:36:10 -0300

    Add effects and blendMode

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,6 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        ZStack {
+            BackCardView()
+                .offset(y: -40)
+                .scaleEffect(0.90)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+            BackCardView()
+                .offset(y: -20)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0.0, z: 0.0))
+                .blendMode(.hardLight)
+            CardView()
+                .blendMode(.hardLight)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
@@ -38,8 +65,14 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(width: 340, height: 220)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
